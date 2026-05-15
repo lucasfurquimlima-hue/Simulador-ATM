2157da6a84a25462b879a3a3080860dd57e1601f
lucasfurquimlima-hue <lucasfurquimlima@gmail.com>
2026-05-15 11:12:27 -0300

    Adicionado void e int main

--- main.c
+++ main.c
@@ -4,3 +4,14 @@
 
 // Definição de constantes
 #define PIN_CORRETO "1234"
+
+// Protótipos das funções (declaração para o compilador conhecer as funções)
+void exibirMenu();
+void realizarDeposito(float *saldo);
+void realizarSaque(float *saldo);
+void consultarSaldo(float saldo);
+int verificarSenha();
+
+int main() {
+    float saldo = 0.0;
+    int opcao;
